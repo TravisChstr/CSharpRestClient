d4f94d13a811755d044bec1ec7612cc9c34f2f0e
Travis Chester <travis.chstr@gmail.com>
2021-03-13 02:41:42 -0500

    Add project files.

---
+++ CSharpRestClient/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CSharpRestClient
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            RestClient restClient = new RestClient();
+            restClient.endPoint = restURI.Text;
+
+            debugOutput("Rest Client Created");
+
+            string strResponse = string.Empty;
+
+            strResponse = restClient.makeRequest();
+
+            debugOutput(strResponse);
+        }
+
+        private void debugOutput(string strDebugTxt)
+        {
+            try
+            {
+                System.Diagnostics.Debug.WriteLine(strDebugTxt);
+                response.Text = response.Text + strDebugTxt + Environment.NewLine;
+                response.SelectionStart = response.SelectionLength;
+                response.ScrollToEnd();
+            }
+            catch(Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine(ex.Message, ToString());
+            }
+        }
+    }
+}
